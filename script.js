77429ea752317d0f16578e5a2288d588726b3fd4
HaipaDev <hypergamesdev@gmail.com>
2023-12-14 15:06:07 +0100

    Rebrand to "HaipaStudios"

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var title="About Us";
 var desc="We're a passionate group thriving<br>\
-to create Hyper cool games and more!<br>\
-Founded by: <b><i><a href='https://hypergamesdev.github.io'>Hyper!</a></i></b>";
-var footer="HyperLemonStudios® 2023 | Maciej Krefft";
+		to create Hyper cool games and more!<br>\
+		Founded by: <b><i><a href='https://haipadev.github.io'>HaipaDev</a></i></b>";
+var footer="HaipaStudios® HaipaDev® Shizuikari® 2023 | Maciej Krefft";
 
 var pageName1="About Us";
 var pageName2="Games";
@@ -16,36 +16,36 @@ setTimeout(function Setup(){
 	$("#music").children("span").html(pageName3);
 	$("#index").attr("href","index.html");
 	//$("#games").attr("href","games.html");
-	$("#games").attr("href","https://hypergamesdev.github.io/portfolio");
+	$("#games").attr("href","https://haipadev.github.io/portfolio");
 	$("#music").attr("href","https://shizuikari.jimdosite.com");
 	$(".footer").children("footer").html(footer);
 	
 	if(page=='games.html'){
 		title=pageName2;
 		desc="";
-		$("#sss222Button").attr("href","https://hypergamesdev.github.io/sss222");
+		$("#sss222Button").attr("href","https://haipadev.github.io/sss222");
 		$("#sss222date").html("03.2020 - ??.????");
-		$("#glitchoutButton").attr("href","https://hypergamesdev.github.io/glitchout");
+		$("#glitchoutButton").attr("href","https://haipadev.github.io/glitchout");
 		$("#glitchoutDate").html("06.2021 - ??.????");
 		$("#rustyropesButton").attr("href","https://github.com/HyperLemonStudios/rusty-ropes");
 		$("#rustyropesDate").html("12.2021 - ??.????");
-		$("#driftakeButton").attr("href","https://github.com/HyperGamesDev/driftake");
+		$("#driftakeButton").attr("href","https://github.com/HaipaDev/driftake");
 		$("#driftakeDate").html("??.???? - ??.????");
 	}else{
 		$("#twitter").html("<img src='img/twitter.png'>");
-		$("#twitter").attr("href","https://twitter.com/HyperrGamesDev");
+		$("#twitter").attr("href","https://twitter.com/HaipaaDev");
 		$("#github").html("<img src='img/github.png'>");
-		$("#github").attr("href","https://github.com/HyperLemonStudios");
+		$("#github").attr("href","https://github.com/HaipaStudios");
 		$("#patreon").html("<img src='img/patreon.png'>");
 		$("#patreon").attr("href","https://patreon.com/HyperGamesDev");
 		$("#kofi").html("<img src='img/kofi.png'>");
 		$("#kofi").attr("href","https://buymeacoffee.com/HyperGamesDev");
 	}
 	$("#title").html(title);
-	$("#desc").html(desc);
+	//$("#desc").html(desc);
 },100);
 
-function redirect_cvEN(){window.open("https://raw.githubusercontent.com/HyperGamesDev/HyperGamesDev/master/Grafika%20Social/CV-HyperGamesDev_EN.png","_self");}
-function redirect_cvPL(){window.open("https://raw.githubusercontent.com/HyperGamesDev/HyperGamesDev/master/Grafika%20Social/CV-HyperGamesDev_PL.png","_self");}
+function redirect_cvEN(){window.open("https://raw.githubusercontent.com/HaipaDev/HaipaDev//master/assets/CV-HaipaDev_EN.pdf","_self");}
+function redirect_cvPL(){window.open("https://raw.githubusercontent.com/HaipaDev/HaipaDev//master/assets/CV-HaipaDev_PL.pdf","_self");}
 
 const delay = async (ms = 1000) => new Promise(resolve => setTimeout(resolve, ms));
